ff9d1edf077a4e0afc5378fe7ad747ce93d23a44
kevyn <161949649+tales04021985@users.noreply.github.com>
2025-08-16 11:02:09 -0300

    Add files via upload

---
+++ java script tales/script.js
@@ -0,0 +1,20 @@
+function ex1(){
+    nome = document.getElementById('ex1-nome').value
+    document.getElementById('ex1-saida').innerHTML = 'Olá '+nome+'!'
+}
+
+function ex2(){
+    // Insira o JavaScript aqui
+}
+
+function ex3(){
+    // Insira o JavaScript aqui
+}
+
+function ex4(){
+    // Insira o JavaScript aqui
+}
+
+function ex5(){
+    // Insira o JavaScript aqui
+}
